4da078b89048501dd62e8f643e76705118c310ef
Julián Nonino <Julin Nonino>
2016-04-11 22:02:18 -0300

    Adding Dockerfile support

--- especialidad/kafka/src/main/java/ar/edu/utn/frc/posgrado/jnonino/Main.java
+++ especialidad/kafka/src/main/java/ar/edu/utn/frc/posgrado/jnonino/Main.java
@@ -11,10 +11,13 @@ public class Main {
 
     public static void main(String [ ] args) {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("client.id", "DemoProducer");
+        props.put("zk.connect", "127.0.0.1:2181");
         props.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
         props.put("value.serializer", "org.apache.kafka.connect.json.JsonSerializer");
+
+//        props.put("bootstrap.servers", "localhost:9092");
+//        props.put("client.id", "DemoProducer");
+
         DataProducer producer = new DataProducer(new KafkaProducer<>(props));
         producer.start();
     }
