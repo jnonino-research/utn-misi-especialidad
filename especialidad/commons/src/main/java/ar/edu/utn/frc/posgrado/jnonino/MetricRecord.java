5c8e5c78bfebed463f3600584bced33e472287b4
Julián Nonino <Julin Nonino>
2016-05-13 18:11:14 -0300

    Starting with Flink app implementation

---
+++ especialidad/commons/src/main/java/ar/edu/utn/frc/posgrado/jnonino/MetricRecord.java
@@ -0,0 +1,83 @@
+package ar.edu.utn.frc.posgrado.jnonino;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Julián on 13/5/2016.
+ */
+public class MetricRecord {
+
+    private final String country;
+    private final String state;
+    private final String city;
+    private final long timestamp;
+    private final float temperature;
+    private final float humidity;
+    private final float pressure;
+    private final float windSpeed;
+    private final String windDirection;
+    private final String owner;
+
+    public MetricRecord(String line) {
+        List<String> values = Arrays.asList(line.split("\\s*,\\s*"));
+        this.country = values.get(0);
+        this.state = values.get(1);
+        this.city = values.get(2);
+        this.timestamp = Long.parseLong(values.get(3));
+        this.temperature = Float.parseFloat(values.get(4));
+        this.humidity = Float.parseFloat(values.get(5));
+        this.pressure = Float.parseFloat(values.get(6));
+        this.windSpeed = Float.parseFloat(values.get(7));
+        this.windDirection = values.get(8);
+        this.owner = values.get(9);
+    }
+
+    public boolean isValidMetric() {
+        boolean invalidMetric = this.country.equalsIgnoreCase("")
+                || this.state.equalsIgnoreCase("")
+                || this.city.equalsIgnoreCase("");
+
+        return !invalidMetric;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }
+
+    public float getWindSpeed() {
+        return windSpeed;
+    }
+
+    public String getWindDirection() {
+        return windDirection;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+}
--- especialidad/flink/src/main/java/ar/edu/utn/frc/posgrado/jnonino/Main.java
+++ especialidad/flink/src/main/java/ar/edu/utn/frc/posgrado/jnonino/Main.java
@@ -1,35 +1,72 @@
 package ar.edu.utn.frc.posgrado.jnonino;
 
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer082;
+import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
 
 /**
  * Created by Julián on 21/3/2016.
+ *
+ * The following parameters should be set
+ *  - "bootstrap.servers" (comma separated list of kafka brokers)
+ *  - "zookeeper.connect" (comma separated list of zookeeper servers)
+ *  - "group.id" the id of the consumer group
+ *  - "topic" the name of the topic to read data from.
+ *
+ * You can pass these required parameters using "--bootstrap.servers host:port,host1:port1 --zookeeper.connect host:port --topic testTopic"
+ *
+ * This is a valid input example:
+ * 		--topic test --bootstrap.servers localhost:9092 --zookeeper.connect localhost:2181 --group.id myGroup
  */
-
 public class Main {
 
     public static void main(String [ ] args){
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        String name = ClassLoader.getSystemResource("numbers.txt").getFile();
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
 
-        DataStream<String> text = env.readTextFile(name);
+        FlinkKafkaConsumer082<String> flinkKafkaConsumer = new FlinkKafkaConsumer082<String>(
+                parameterTool.getRequired("topic"),
+                new SimpleStringSchema(),
+                parameterTool.getProperties());
 
-        DataStream<String> parsed = text.map(new MapFunction<String, String>() {
-            public String map(String value) {
-                return "Number: " + value;
-            }
-        });
+        DataStream<String> messageStream = env.addSource(flinkKafkaConsumer);
 
-        parsed.print();
+        messageStream.rebalance().map((MapFunction<String, MetricRecord>) s -> {
+            MetricRecord metricRecord = new MetricRecord(s);
+            return metricRecord;
+        }).print();
 
-        try {
-            env.execute();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+//        messageStream.rebalance().map(new MapFunction<String, String>() {
+//            private static final long serialVersionUID = -6867736771747690202L;
+//
+//            @Override
+//            public String map(String value) throws Exception {
+//                return "Kafka and Flink says: " + value;
+//            }
+//        }).print();
+
+
+        // Process numbers.txt file
+//        String name = ClassLoader.getSystemResource("numbers.txt").getFile();
+//        DataStream<String> text = env.readTextFile(name);
+//        DataStream<String> parsed = text.map(new MapFunction<String, String>() {
+//            public String map(String value) {
+//                return "Number: " + value;
+//            }
+//        });
+//        parsed.print();
+//
+//
+//        try {
+//            env.execute();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
 
 
