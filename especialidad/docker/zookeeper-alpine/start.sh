acac14fd7c30596ffab730aa03d6378d25dcccf4
Julian Nonino <noninojulian@outlook.com>
2016-06-27 20:00:01 -0300

    Working on Docker Images

--- especialidad/docker/zookeeper-alpine/start.sh
+++ especialidad/docker/zookeeper-alpine/start.sh
@@ -6,6 +6,5 @@ if [[ -z "${ZK_ID}" || -z "${ZK_SERVERS}" ]]; then
 fi
 
 echo "${ZK_SERVERS}" | tr ' ' '\n' | tee -a /opt/zookeeper/conf/zoo.cfg
-echo "${ZK_ID}" | tee /tmp/zookeeper/myid
+echo "${ZK_ID}" | tee /opt/zookeeper/data/myid
 /opt/zookeeper/bin/zkServer.sh start-foreground
-
