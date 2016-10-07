9344626223248b335a931aada7ce8f1479fd3c5c
Julian Nonino <noninojulian@outlook.com>
2016-10-06 21:57:40 -0300

    Creando script para buildear todas las imagenes de docker al mismo tiempo

---
+++ trabajo/docker_images/build_all_images.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker build -t jnonino/zookeeper zookeeper
+docker build -t jnonino/kafka kafka
+docker build -t jnonino/storm-base storm/storm-base
+docker build -t jnonino/storm-nimbus storm/storm-nimbus
+docker build -t jnonino/storm-supervisor storm/storm-supervisor
+docker build -t jnonino/storm-ui storm/storm-ui
